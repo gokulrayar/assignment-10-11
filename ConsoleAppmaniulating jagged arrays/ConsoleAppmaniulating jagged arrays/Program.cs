39567fa7ac94e27e23911add6894db36dc11016c
gokulrayar <gokulrayar>
2023-11-13 15:45:24 +0530

    Assignment 10-11

---
+++ ConsoleAppmaniulating jagged arrays/ConsoleAppmaniulating jagged arrays/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppmaniulating_jagged_arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a jagged array called StudentScore to store the scores of three students
+            int[][] StudentScore = new int[3][];
+
+            // Initialize the StudentScore array with the following scores
+            StudentScore[0] = new int[] { 85, 92, 78 };
+            StudentScore[1] = new int[] { 90, 87, 93, 89 };
+            StudentScore[2] = new int[] { 76, 88 };
+
+            // Print the scores of each student using nested loops
+            for (int i = 0; i < StudentScore.Length; i++)
+            {
+                Console.Write("Student {0}: ", i + 1);
+                for (int j = 0; j < StudentScore[i].Length; j++)
+                {
+                    Console.Write("{0} ", StudentScore[i][j]);
+                }
+                Console.WriteLine();
+            }
+
+            // Calculate the average score for each student and print the results
+            for (int i = 0; i < StudentScore.Length; i++)
+            {
+                int sum = 0;
+                for (int j = 0; j < StudentScore[i].Length; j++)
+                {
+                    sum += StudentScore[i][j];
+                }
+                double average = (double)sum / StudentScore[i].Length;
+                Console.WriteLine("Average score for Student {0}: {1}", i + 1, average);
+            }
+
+            // Calculate the average score for all the students combined and print the result
+            int totalSum = 0;
+            int totalCount = 0;
+            for (int i = 0; i < StudentScore.Length; i++)
+            {
+                for (int j = 0; j < StudentScore[i].Length; j++)
+                {
+                    totalSum += StudentScore[i][j];
+                    totalCount++;
+                }
+            }
+            double totalAverage = (double)totalSum / totalCount;
+            Console.WriteLine("Average score for all students combined: {0}", totalAverage);
+            Console.ReadKey();
+        }
+    }
+
+
+}
+    
---
+++ method and parameters-salary employee/method and parameters-salary employee/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace method_and_parameters_salary_employee
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose Employee Type:");
+            Console.WriteLine("1. HR");
+            Console.WriteLine("2. Admin");
+            Console.WriteLine("3. Software Developer");
+
+            int employeeType = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter working hours:");
+            int workingHours = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter number of working days:");
+            int numWorkingDays = int.Parse(Console.ReadLine());
+
+            int projectHandles = 0;
+            int extras = 0;
+
+            if (employeeType == 2 || employeeType == 3)
+            {
+                Console.WriteLine("Enter project handles:");
+                projectHandles = int.Parse(Console.ReadLine());
+            }
+
+            if (employeeType == 3)
+            {
+                Console.WriteLine("Enter extra hours:");
+                extras = int.Parse(Console.ReadLine());
+            }
+
+            int monthlySalary = CalculateMonthlySalary(employeeType, workingHours, numWorkingDays, projectHandles, extras);
+
+            Console.WriteLine($"Monthly Salary: {monthlySalary}");
+        }
+
+        static int CalculateMonthlySalary(int employeeType, int workingHours, int numWorkingDays, int projectHandles = 0, int extras = 0)
+        {
+            int baseSalary = workingHours * numWorkingDays * 100;
+
+            if (employeeType == 1 || employeeType == 2)
+            {
+                return baseSalary + projectHandles * 3000;
+            }
+            else if (employeeType == 3)
+            {
+                return baseSalary + projectHandles * 3000 + extras * 2000;
+            }
+            else
+            {
+                throw new ArgumentException("Invalid employee type");
+            }
+        }
+    }
+}
+    
